7a6a208c02aa9eece16d75496cee332212d1b96c
uday kiran r <uday6794@gmail.com>
2019-06-02 12:04:31 +0530

    snd commit

---
+++ tst.js
@@ -0,0 +1,20 @@
+var sum = 0;
+u = [1,23,47,12,56,135,2];
+
+for (let i = 0; i < u.length; i++) {
+    console.log(u[i])
+    sum += u[i]
+    
+}
+console.log("sum is :"+sum);
+
+// object data type is similar to dictionaries
+
+df = {
+    "name" : "DJ",
+    "age" : 24,
+}
+
+for(data in df){
+    console.log(data +':' + df[data])
+}
